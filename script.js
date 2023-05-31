d6406cba22658f2021ec1f180980afe9cd54712f
Vlad Balas <balash_vlad@yahoo.com>
2023-05-31 15:21:27 +0100

    Form Validation

--- script.js
+++ script.js
@@ -16,3 +16,38 @@ function showModal() {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal')); // hide modal container when 'X' is clicked
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate Form
+function validate(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g; // Regular Expression for ensuring URL starts with HTTP/HTTPS
+    const regex = new RegExp(expression);
+    // Check if user filled in both fields
+    if (!nameValue || !urlValue) {
+        alert('Please submit values for both fields!');
+        return false;
+    }
+    // Check if web address is valid
+    if (!urlValue.match(regex)) {
+        alert('Please provide a valid web address');
+        return false;
+    }
+    // Valid
+    return true;
+}
+
+// Handle Data from Form
+function storeBookmark(e) {
+    e.preventDefault(); // prevent it from submitting the form to a webserver as we need to chatch the values within the JavaScript
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+        urlValue = `https://${urlValue}`;
+    }
+    console.log(nameValue, urlValue);
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    }
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
